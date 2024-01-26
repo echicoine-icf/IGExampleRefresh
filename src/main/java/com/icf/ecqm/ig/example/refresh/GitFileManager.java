144d83669eafa2168e06858ab01b75e271b2c646
Evan Chicoine <evan.chicoine@esacinc.com>
2024-01-25 19:17:05 -0500

    Working so far...

--- src/main/java/com/icf/ecqm/ig/example/refresh/GitFileManager.java
+++ src/main/java/com/icf/ecqm/ig/example/refresh/GitFileManager.java
@@ -10,43 +10,39 @@ import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
 import com.google.gson.*;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 public class GitFileManager {
     static String repoURL = "https://github.com/echicoine-icf/IGExampleRefresh.git";
-    static String folderPath = "src/main/java/resources/SAMPLE";
+    static String folderPath = "src/main/resources/SAMPLE";
     static String localFolderPath = "input/resources";
-
+    static String examplesFilePath = "input/pagecontent/examples.md";
     //Clone repo to temp folder which gets deleted when we're done:
 
     public static void processExampleFiles() {
         // Step 1: Clone the repository to a temporary folder
-        String tempFolder1 = "path/to/temp/folder1";
+        String repoLocation = "temp/repo";
+        String filesMovedLocation = "temp/files";
+        deleteFolder(new File("temp"));
 
         try (Git git = Git.cloneRepository()
                 .setURI(repoURL)
-                .setDirectory(new File(tempFolder1))
+                .setDirectory(new File(repoLocation))
                 .call()) {
 
-            // Step 2: Transfer contents of src/main/java/resources/QI-CORE to a new temporary folder
-            String tempFolder2 = "path/to/temp/folder2";
-            File sourceFolder = new File(tempFolder1, folderPath);
-            File destFolder = new File(tempFolder2);
+            // Step 2: Transfer contents of folderPath to a new temporary folder
+            File sourceFolder = new File(repoLocation, folderPath);
+            File destFolder = new File(filesMovedLocation);
             FileUtils.copyDirectory(sourceFolder, destFolder);
 
             // Step 3: Process the new temporary folder, transfer files to input/resources, and create a hashmap
             Map<String, List<Pair<String, JsonObject>>> folderContentsMap = new HashMap<>();
             File[] folders = destFolder.listFiles(File::isDirectory);
+            System.out.println(Arrays.toString(folders));
             if (folders != null) {
                 for (File subfolder : folders) {
-                    File destSubfolder = new File(subfolder, localFolderPath);
+                    File destSubfolder = new File(localFolderPath);
 
                     //put the json files into the resource folder:
                     FileUtils.copyDirectory(subfolder, destSubfolder);
@@ -68,31 +64,64 @@ public class GitFileManager {
             }
 
             // Step 4: create example.md page:
-
-            StringBuilder sb = new StringBuilder();
-
-
+            StringBuilder examplesMDContent = new StringBuilder();
             for (Map.Entry<String, List<Pair<String, JsonObject>>> entry : folderContentsMap.entrySet()) {
-
-                sb.append("\r\n**" + entry.getKey() + "**");
-
+                examplesMDContent.append("\r\n**" + entry.getKey() + "**");
                 for (Pair<String, JsonObject> jsonPair : entry.getValue()) {
                     JsonObject jsonObject = jsonPair.getRight();
                     String fileName = jsonPair.getLeft();
                     String exampleTitle = jsonObject.get("title").getAsString();
-
-                    sb.append("\n* [" + exampleTitle + "](" + fileName.replace(".json", ".html") + ")");
+                    examplesMDContent.append("\n* [" + exampleTitle + "](" + fileName.replace(".json", ".html") + ")");
                 }
+                System.out.println(examplesMDContent);
 
-
-                System.out.println(sb);
+                // Specify the path to the file
+                File examplesMDFileLocationCurrent = new File(examplesFilePath);
+                if (!examplesMDFileLocationCurrent.exists()){
+                    examplesMDFileLocationCurrent.getParentFile().mkdirs();
+                }
+                // Write the content to the file
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(examplesFilePath))) {
+                    writer.write(examplesMDContent.toString());
+                    System.out.println("Content written to file successfully!");
+                } catch (IOException e) {
+                    System.out.println("Error writing to file: " + e.getMessage());
+                }
             }
 
-            // Step 5: Delete temporary folders
-            FileUtils.deleteDirectory(new File(tempFolder1));
-            FileUtils.deleteDirectory(destFolder);
         } catch (Exception e) {
             e.printStackTrace();
+
+        }
+        //cleanup
+        deleteFolder(new File("temp"));
+    }
+
+
+    private static boolean deleteFolder(File folder) {
+        if (folder.exists()) {
+            File[] files = folder.listFiles();
+
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        // Recursive call to delete subdirectory
+                        deleteFolder(file);
+                    } else {
+                        // Delete file
+                        if (!file.delete()) {
+                            System.out.println("Failed to delete file: " + file.getAbsolutePath());
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            // Delete the empty folder
+            return folder.delete();
+        } else {
+            System.out.println("Folder does not exist: " + folder.getAbsolutePath());
+            return false;
         }
     }
 
